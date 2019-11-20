33f979b2562458a83a8e36a8710422c64700cab5
Leo GIGANT <leo.gigant@ynov.com>
2019-11-20 16:04:15 +0100

    Creation de la vue de login et creation du formulaire de connexion

---
+++ SelfBuy/Screens/SignIn/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  SelfBuy
+//
+//  Created by Coline Rousselot on 20/11/2019.
+//  Copyright © 2019 MaxenceMottard. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SelfBuy/Screens/SignIn/Views/SignInView.swift
@@ -0,0 +1,21 @@
+//
+//  SignInView.swift
+//  SelfBuy
+//
+//  Created by Coline Rousselot on 20/11/2019.
+//  Copyright © 2019 MaxenceMottard. All rights reserved.
+//
+
+import UIKit
+
+class SignInView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

5d4ca7eb7a08a9d3d0a7e138e3121c464da48dea
Zakarya Tolba <zakarya.tolba@ynov.com>
2019-11-29 23:55:27 +0100

    Added the profile picture ImageView (remains to be centered), added separator line between buttons, fixed button activation zone

--- SelfBuy/Screens/Profile/ProfileViewController.swift
+++ SelfBuy/Screens/Profile/ProfileViewController.swift
@@ -10,17 +10,12 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-//    var profileView: ProfileView = {
-//        let view = UIView()
-//        view.alpha = 0
-//        view.setController(controller: self)
-//        view.setupLayout()
-//    }
-    
-    var profileView: ProfileView!
-    
-    // Temp string profileName
+    // Temp strings for profileName & avatarFileName
+    let avatarFileName = "avatarZak.png"
     let profileName = "Zakarya Tolba"
+    // Button arrow
+    let arrow_right = UIImage(named: "arrow_right.png")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,24 +33,33 @@ class ProfileViewController: UIViewController {
             
          }
         
-//        self.view.addSubview(profileView!)
-        
         // PROFILE PICTURE
-        
-        //Temp profile picture
-        
         let profilePictureView = UIImageView()
-        profilePictureView.frame = CGRect(x: 200, y: 200, width: 200, height: 200)
-        profilePictureView.image = UIImage(named: "avatarZak.png")
-//        profilePictureView.imgViewCorners()
+        profilePictureView.frame = CGRect(x: UIScreen.main.bounds.width / 2, y: 150, width: 150, height: 150)
+        profilePictureView.image = UIImage(named: avatarFileName)
+        profilePictureView.layer.cornerRadius = 75
+        profilePictureView.layer.masksToBounds = true
         self.view.addSubview(profilePictureView)
-        
-        
-        let arrow_right = UIImage(named: "arrow_right.png")
+    
+   
+        // PROFILE NAME LABEL
+        let profileNameLabel = UILabel()
+        profileNameLabel.textColor = .black
+        profileNameLabel.text = profileName
+        profileNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        profileNameLabel.textAlignment = .center
+        profileNameLabel.font = UIFont(name:"Helvetica", size: 20.0)
+        self.view.addSubview(profileNameLabel)
+              
+        NSLayoutConstraint.activate([
+               profileNameLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 190.0),
+               profileNameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16.0),
+               profileNameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16.0)
+        ])
         
         // ACCOUNT BUTTON
         let accountButton = UIButton()
-        accountButton.setTitleColor(.gray, for: .normal)
+        accountButton.setTitleColor(UIColor.gray, for: .normal)
         accountButton.setTitleColor(.black, for: .highlighted)
         accountButton.addTarget(self, action: #selector(pushAccountButtonAction), for: .touchUpInside)
         accountButton.setTitle("Account", for: .normal)
@@ -65,82 +69,71 @@ class ProfileViewController: UIViewController {
         
         accountButton.setImage(arrow_right, for: .normal)
         accountButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-        accountButton.imageView?.centerYAnchor.constraint(equalTo: accountButton.centerYAnchor, constant: 0.0).isActive = true
+        accountButton.imageView?.centerYAnchor.constraint(equalTo: accountButton.centerYAnchor, constant: 70.0).isActive = true
         accountButton.imageView?.trailingAnchor.constraint(equalTo: accountButton.trailingAnchor, constant: -20).isActive = true
 
         NSLayoutConstraint.activate([
                  accountButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
                  accountButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-                 accountButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 50.0),
-                 accountButton.heightAnchor.constraint(equalToConstant: 1000.0),
+                 accountButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -290.0),
+                 accountButton.heightAnchor.constraint(equalToConstant: 48.0),
        ])
         
+        let lineView = UIView(frame: CGRect(x: 30, y: 450, width: 300, height: 1.0))
+        lineView.layer.borderWidth = 1.0
+        lineView.layer.borderColor = UIColor.gray.cgColor
+        self.view.addSubview(lineView)
         
-        // ORDERS BUTTON
-        let ordersButton = UIButton()
-        ordersButton.setTitleColor(.gray, for: .normal)
-        ordersButton.setTitleColor(.black, for: .highlighted)
-        ordersButton.addTarget(self, action: #selector(pushOrdersButtonAction), for: .touchUpInside)
-        ordersButton.setTitle("Orders", for: .normal)
-        ordersButton.translatesAutoresizingMaskIntoConstraints = false
-        ordersButton.semanticContentAttribute = .forceRightToLeft
-        self.view.addSubview(ordersButton)
-        
-        
-        ordersButton.setImage(arrow_right, for: .normal)
-        ordersButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-        ordersButton.imageView?.centerYAnchor.constraint(equalTo: ordersButton.centerYAnchor, constant: 0.0).isActive = true
-        ordersButton.imageView?.trailingAnchor.constraint(equalTo: ordersButton.trailingAnchor, constant: -20).isActive = true
-
-         NSLayoutConstraint.activate([
-                  ordersButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
-                  ordersButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-                  ordersButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 100.0),
-                  ordersButton.heightAnchor.constraint(equalToConstant: 1000.0),
-        ])
-        
+//        // ORDERS BUTTON
+//        let ordersButton = UIButton()
+//        ordersButton.setTitleColor(UIColor.gray, for: .normal)
+//        ordersButton.setTitleColor(.black, for: .highlighted)
+//        ordersButton.addTarget(self, action: #selector(pushOrdersButtonAction), for: .touchUpInside)
+//        ordersButton.setTitle("Orders", for: .normal)
+//        ordersButton.translatesAutoresizingMaskIntoConstraints = false
+//        ordersButton.semanticContentAttribute = .forceRightToLeft
+//        self.view.addSubview(ordersButton)
+//
+//
+//        ordersButton.setImage(arrow_right, for: .normal)
+//        ordersButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+//        ordersButton.imageView?.centerYAnchor.constraint(equalTo: ordersButton.centerYAnchor, constant: 0.0).isActive = true
+//        ordersButton.imageView?.trailingAnchor.constraint(equalTo: ordersButton.trailingAnchor, constant: -20).isActive = true
+//
+//         NSLayoutConstraint.activate([
+//                  ordersButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+//                  ordersButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+//                  ordersButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 100.0),
+//                  ordersButton.heightAnchor.constraint(equalToConstant: 1000.0),
+//        ])
+//
         
         // PAYMENT BUTTON
-        let paymentButton = UIButton()
-        paymentButton.setTitleColor(.gray, for: .normal)
-        paymentButton.setTitleColor(.black, for: .highlighted)
-        paymentButton.addTarget(self, action: #selector(pushPaymentButtonAction), for: .touchUpInside)
-        paymentButton.setTitle("Payment", for: .normal)
-        paymentButton.translatesAutoresizingMaskIntoConstraints = false
-        paymentButton.semanticContentAttribute = .forceRightToLeft
-        self.view.addSubview(paymentButton)
+//        let paymentButton = UIButton()
+//        paymentButton.setTitleColor(UIColor.gray, for: .normal)
+//        paymentButton.setTitleColor(.black, for: .highlighted)
+//        paymentButton.addTarget(self, action: #selector(pushPaymentButtonAction), for: .touchUpInside)
+//        paymentButton.setTitle("Payment", for: .normal)
+//        paymentButton.translatesAutoresizingMaskIntoConstraints = false
+//        paymentButton.semanticContentAttribute = .forceRightToLeft
+//        self.view.addSubview(paymentButton)
+//
+//
+//        paymentButton.setImage(arrow_right, for: .normal)
+//        paymentButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+//        paymentButton.imageView?.centerYAnchor.constraint(equalTo: paymentButton.centerYAnchor, constant: 0.0).isActive = true
+//        paymentButton.imageView?.trailingAnchor.constraint(equalTo: paymentButton.trailingAnchor, constant: -20).isActive = true
+//
+//         NSLayoutConstraint.activate([
+//                  paymentButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+//                  paymentButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+//                  paymentButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 150.0),
+//                  paymentButton.heightAnchor.constraint(equalToConstant: 1000.0),
+//        ])
 
         
-        paymentButton.setImage(arrow_right, for: .normal)
-        paymentButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-        paymentButton.imageView?.centerYAnchor.constraint(equalTo: paymentButton.centerYAnchor, constant: 0.0).isActive = true
-        paymentButton.imageView?.trailingAnchor.constraint(equalTo: paymentButton.trailingAnchor, constant: -20).isActive = true
+      
 
-         NSLayoutConstraint.activate([
-                  paymentButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
-                  paymentButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-                  paymentButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 150.0),
-                  paymentButton.heightAnchor.constraint(equalToConstant: 1000.0),
-        ])
-
-        
-        // LABEL PROFILE NAME
-        let profileNameLabel = UILabel()
-        profileNameLabel.textColor = .black
-        profileNameLabel.text = profileName
-        profileNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        profileNameLabel.textAlignment = .center
-        self.view.addSubview(profileNameLabel)
-        
-        NSLayoutConstraint.activate([
-             profileNameLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
-             profileNameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16.0),
-             profileNameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16.0)
-         ])
-
-     
-          
-        
     }
     
   

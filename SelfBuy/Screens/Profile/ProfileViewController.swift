cadbe1b343a09beceb9d808e95b25d89153cae91
Zakarya Tolba <zakarya.tolba@ynov.com>
2019-11-30 10:48:46 +0100

    Removed profile picture, added Privacy and Policy button, added About Us button

--- SelfBuy/Screens/Profile/ProfileViewController.swift
+++ SelfBuy/Screens/Profile/ProfileViewController.swift
@@ -11,11 +11,10 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     // Temp strings for profileName & avatarFileName
-    let avatarFileName = "avatarZak.png"
+//    let avatarFileName = "avatarZak.png"
     let profileName = "Zakarya Tolba"
     // Button arrow
     let arrow_right = UIImage(named: "arrow_right.png")
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,13 +33,13 @@ class ProfileViewController: UIViewController {
          }
         
         // PROFILE PICTURE
-        let profilePictureView = UIImageView()
-        profilePictureView.frame = CGRect(x: UIScreen.main.bounds.width / 2, y: 150, width: 150, height: 150)
-        profilePictureView.image = UIImage(named: avatarFileName)
-        profilePictureView.layer.cornerRadius = 75
-        profilePictureView.layer.masksToBounds = true
-        self.view.addSubview(profilePictureView)
-    
+//        let profilePictureView = UIImageView()
+//        profilePictureView.frame = CGRect(x: UIScreen.main.bounds.width / 2 - 60, y: 150, width: 120, height: 120)
+//        profilePictureView.image = UIImage(named: avatarFileName)
+//        profilePictureView.layer.cornerRadius = 60
+//        profilePictureView.layer.masksToBounds = true
+//        self.view.addSubview(profilePictureView)
+//
    
         // PROFILE NAME LABEL
         let profileNameLabel = UILabel()
@@ -50,107 +49,179 @@ class ProfileViewController: UIViewController {
         profileNameLabel.textAlignment = .center
         profileNameLabel.font = UIFont(name:"Helvetica", size: 20.0)
         self.view.addSubview(profileNameLabel)
+    
               
         NSLayoutConstraint.activate([
-               profileNameLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 190.0),
+               profileNameLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 150.0),
                profileNameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16.0),
                profileNameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16.0)
         ])
         
-        // ACCOUNT BUTTON
-        let accountButton = UIButton()
-        accountButton.setTitleColor(UIColor.gray, for: .normal)
-        accountButton.setTitleColor(.black, for: .highlighted)
-        accountButton.addTarget(self, action: #selector(pushAccountButtonAction), for: .touchUpInside)
-        accountButton.setTitle("Account", for: .normal)
-        accountButton.translatesAutoresizingMaskIntoConstraints = false
-        accountButton.semanticContentAttribute = .forceRightToLeft
-        self.view.addSubview(accountButton)
+        // PROFILE EMAIL LABEL
         
-        accountButton.setImage(arrow_right, for: .normal)
-        accountButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-        accountButton.imageView?.centerYAnchor.constraint(equalTo: accountButton.centerYAnchor, constant: 70.0).isActive = true
-        accountButton.imageView?.trailingAnchor.constraint(equalTo: accountButton.trailingAnchor, constant: -20).isActive = true
+        
+        // ACCOUNT BUTTON
+        let accountProfileButton = UIButton()
+        accountProfileButton.setTitleColor(UIColor.gray, for: .normal)
+        accountProfileButton.setTitleColor(.black, for: .highlighted)
+        accountProfileButton.addTarget(self, action: #selector(pushProfileAccountButtonAction), for: .touchUpInside)
+        accountProfileButton.setTitle("Account", for: .normal)
+        accountProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        accountProfileButton.semanticContentAttribute = .forceRightToLeft
+        self.view.addSubview(accountProfileButton)
+        
+        accountProfileButton.setImage(arrow_right, for: .normal)
+        accountProfileButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+        accountProfileButton.imageView?.centerYAnchor.constraint(equalTo: accountProfileButton.centerYAnchor, constant: 80.0).isActive = true
+        accountProfileButton.imageView?.trailingAnchor.constraint(equalTo: accountProfileButton.trailingAnchor, constant: -20).isActive = true
 
         NSLayoutConstraint.activate([
-                 accountButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
-                 accountButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-                 accountButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -290.0),
-                 accountButton.heightAnchor.constraint(equalToConstant: 48.0),
+                 accountProfileButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+                 accountProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+                 accountProfileButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -310.0),
+                 accountProfileButton.heightAnchor.constraint(equalToConstant: 48.0),
        ])
         
-        let lineView = UIView(frame: CGRect(x: 30, y: 450, width: 300, height: 1.0))
-        lineView.layer.borderWidth = 1.0
-        lineView.layer.borderColor = UIColor.gray.cgColor
-        self.view.addSubview(lineView)
+        let lineView1 = UIView(frame: CGRect(x: 35, y: 430, width: 300, height: 1.0))
+        lineView1.layer.borderWidth = 1.0
+        lineView1.layer.borderColor = UIColor.gray.cgColor
+        self.view.addSubview(lineView1)
         
-//        // ORDERS BUTTON
-//        let ordersButton = UIButton()
-//        ordersButton.setTitleColor(UIColor.gray, for: .normal)
-//        ordersButton.setTitleColor(.black, for: .highlighted)
-//        ordersButton.addTarget(self, action: #selector(pushOrdersButtonAction), for: .touchUpInside)
-//        ordersButton.setTitle("Orders", for: .normal)
-//        ordersButton.translatesAutoresizingMaskIntoConstraints = false
-//        ordersButton.semanticContentAttribute = .forceRightToLeft
-//        self.view.addSubview(ordersButton)
-//
-//
-//        ordersButton.setImage(arrow_right, for: .normal)
-//        ordersButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-//        ordersButton.imageView?.centerYAnchor.constraint(equalTo: ordersButton.centerYAnchor, constant: 0.0).isActive = true
-//        ordersButton.imageView?.trailingAnchor.constraint(equalTo: ordersButton.trailingAnchor, constant: -20).isActive = true
-//
-//         NSLayoutConstraint.activate([
-//                  ordersButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
-//                  ordersButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-//                  ordersButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 100.0),
-//                  ordersButton.heightAnchor.constraint(equalToConstant: 1000.0),
-//        ])
-//
+        // ORDERS BUTTON
+        let ordersProfileButton = UIButton()
+        ordersProfileButton.setTitleColor(UIColor.gray, for: .normal)
+        ordersProfileButton.setTitleColor(.black, for: .highlighted)
+        ordersProfileButton.addTarget(self, action: #selector(pushProfileOrdersButtonAction), for: .touchUpInside)
+        ordersProfileButton.setTitle("Orders", for: .normal)
+        ordersProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        ordersProfileButton.semanticContentAttribute = .forceRightToLeft
+        self.view.addSubview(ordersProfileButton)
+
+
+        ordersProfileButton.setImage(arrow_right, for: .normal)
+        ordersProfileButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+        ordersProfileButton.imageView?.centerYAnchor.constraint(equalTo: ordersProfileButton.centerYAnchor, constant: 70.0).isActive = true
+        ordersProfileButton.imageView?.trailingAnchor.constraint(equalTo: ordersProfileButton.trailingAnchor, constant: -20).isActive = true
+
+
+        NSLayoutConstraint.activate([
+          ordersProfileButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+          ordersProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+          ordersProfileButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -245.0),
+          ordersProfileButton.heightAnchor.constraint(equalToConstant: 48.0),
+        ])
+        
+        let lineView2 = UIView(frame: CGRect(x: 35, y: 490, width: 300, height: 1.0))
+        lineView2.layer.borderWidth = 1.0
+        lineView2.layer.borderColor = UIColor.gray.cgColor
+        self.view.addSubview(lineView2)
         
         // PAYMENT BUTTON
-//        let paymentButton = UIButton()
-//        paymentButton.setTitleColor(UIColor.gray, for: .normal)
-//        paymentButton.setTitleColor(.black, for: .highlighted)
-//        paymentButton.addTarget(self, action: #selector(pushPaymentButtonAction), for: .touchUpInside)
-//        paymentButton.setTitle("Payment", for: .normal)
-//        paymentButton.translatesAutoresizingMaskIntoConstraints = false
-//        paymentButton.semanticContentAttribute = .forceRightToLeft
-//        self.view.addSubview(paymentButton)
-//
-//
-//        paymentButton.setImage(arrow_right, for: .normal)
-//        paymentButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
-//        paymentButton.imageView?.centerYAnchor.constraint(equalTo: paymentButton.centerYAnchor, constant: 0.0).isActive = true
-//        paymentButton.imageView?.trailingAnchor.constraint(equalTo: paymentButton.trailingAnchor, constant: -20).isActive = true
-//
-//         NSLayoutConstraint.activate([
-//                  paymentButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
-//                  paymentButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
-//                  paymentButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 150.0),
-//                  paymentButton.heightAnchor.constraint(equalToConstant: 1000.0),
-//        ])
+        let paymentProfileButton = UIButton()
+        paymentProfileButton.setTitleColor(UIColor.gray, for: .normal)
+        paymentProfileButton.setTitleColor(.black, for: .highlighted)
+        paymentProfileButton.addTarget(self, action: #selector(pushProfilePaymentButtonAction), for: .touchUpInside)
+        paymentProfileButton.setTitle("Payment", for: .normal)
+        paymentProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        paymentProfileButton.semanticContentAttribute = .forceRightToLeft
+        self.view.addSubview(paymentProfileButton)
+
+
+        paymentProfileButton.setImage(arrow_right, for: .normal)
+        paymentProfileButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+        paymentProfileButton.imageView?.centerYAnchor.constraint(equalTo: paymentProfileButton.centerYAnchor, constant: 0.0).isActive = true
+        paymentProfileButton.imageView?.trailingAnchor.constraint(equalTo: paymentProfileButton.trailingAnchor, constant: -20).isActive = true
+
+         NSLayoutConstraint.activate([
+                  paymentProfileButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+                  paymentProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+                  paymentProfileButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -185.0),
+                  paymentProfileButton.heightAnchor.constraint(equalToConstant: 48.0),
+        ])
+
+        let lineView3 = UIView(frame: CGRect(x: 35, y: 550, width: 300, height: 1.0))
+        lineView3.layer.borderWidth = 1.0
+        lineView3.layer.borderColor = UIColor.gray.cgColor
+        self.view.addSubview(lineView3)
+        
+        // PRIVACY AND POLICY BUTTON
+        let ppProfileButton = UIButton()
+        ppProfileButton.setTitleColor(UIColor.gray, for: .normal)
+        ppProfileButton.setTitleColor(.black, for: .highlighted)
+        ppProfileButton.addTarget(self, action: #selector(pushPrivacyPButtonAction), for: .touchUpInside)
+        ppProfileButton.setTitle("Privacy & Policy", for: .normal)
+        ppProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        ppProfileButton.semanticContentAttribute = .forceRightToLeft
+        self.view.addSubview(ppProfileButton)
+
+
+        ppProfileButton.setImage(arrow_right, for: .normal)
+        ppProfileButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+        ppProfileButton.imageView?.centerYAnchor.constraint(equalTo: ppProfileButton.centerYAnchor, constant: 0.0).isActive = true
+        ppProfileButton.imageView?.trailingAnchor.constraint(equalTo: ppProfileButton.trailingAnchor, constant: -20).isActive = true
+
+        NSLayoutConstraint.activate([
+                   ppProfileButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+                   ppProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+                   ppProfileButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -125.0),
+                   ppProfileButton.heightAnchor.constraint(equalToConstant: 48.0),
+        ])
+        
+        let lineView4 = UIView(frame: CGRect(x: 35, y: 610, width: 300, height: 1.0))
+        lineView4.layer.borderWidth = 1.0
+        lineView4.layer.borderColor = UIColor.gray.cgColor
+        self.view.addSubview(lineView4)
+        
+        
+        // ABOUT US BUTTON
+        
+        let aboutUsProfileButton = UIButton()
+        aboutUsProfileButton.setTitleColor(UIColor.gray, for: .normal)
+        aboutUsProfileButton.setTitleColor(.black, for: .highlighted)
+        aboutUsProfileButton.addTarget(self, action: #selector(pushPrivacyPButtonAction), for: .touchUpInside)
+        aboutUsProfileButton.setTitle("About Us", for: .normal)
+        aboutUsProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        aboutUsProfileButton.semanticContentAttribute = .forceRightToLeft
+        self.view.addSubview(aboutUsProfileButton)
+
+        aboutUsProfileButton.setImage(arrow_right, for: .normal)
+        aboutUsProfileButton.imageView?.translatesAutoresizingMaskIntoConstraints = true
+        aboutUsProfileButton.imageView?.centerYAnchor.constraint(equalTo: aboutUsProfileButton.centerYAnchor, constant: 0.0).isActive = true
+        aboutUsProfileButton.imageView?.trailingAnchor.constraint(equalTo: aboutUsProfileButton.trailingAnchor, constant: -20).isActive = true
+
+        NSLayoutConstraint.activate([
+                  aboutUsProfileButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -100.0),
+                  aboutUsProfileButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -48.0),
+                  aboutUsProfileButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -65.0),
+                  aboutUsProfileButton.heightAnchor.constraint(equalToConstant: 48.0),
+        ])
+        
+        
+        // SIGN OUT BUTTON
 
         
-      
-
+        
     }
     
   
-    @objc private func pushAccountButtonAction() {
+    @objc private func pushProfileAccountButtonAction() {
         // no implementation
     }
-    
-    @objc private func pushPaymentButtonAction() {
+
+     
+    @objc private func pushProfileOrdersButtonAction() {
          // no implementation
      }
      
-    @objc private func pushOrdersButtonAction() {
+
+    @objc private func pushProfilePaymentButtonAction() {
          // no implementation
      }
-     
     
     
+    @objc private func pushPrivacyPButtonAction() {
+         // no implementation
+     }
     
     
     /*

1462acb41279bc4558b6b0208603c2de21ac2a0d
MaxenceMottard <mottard.maxence@gmail.com>
2019-12-03 17:47:11 +0100

    Update color with logo

--- SelfBuy/Additions/Extensions/UIColorExtensions.swift
+++ SelfBuy/Additions/Extensions/UIColorExtensions.swift
@@ -11,7 +11,8 @@ import UIKit
 
 extension UIColor {
     
-    static let primary = UIColor(displayP3Red: 146/255, green: 104/255, blue: 72/255, alpha: 1)
+    static let primary = UIColor(displayP3Red: 245/255, green: 162/255, blue: 22/255, alpha: 1)
+    static let secondary = UIColor(displayP3Red: 246/255, green: 227/255, blue: 41/255, alpha: 1)
     static let darkGray = UIColor(displayP3Red: 211/255, green: 211/255, blue: 211/255, alpha: 1)
     static let lightGray = UIColor(displayP3Red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
     static let black = UIColor(displayP3Red: 45/255, green: 45/255, blue: 45/255, alpha: 1)

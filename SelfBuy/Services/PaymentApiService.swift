552e036cc41ca578b922db4a248a19faf15b3bb7
Léo GIGANT <rowix4@gmail.com>
2020-03-25 17:01:41 +0100

    Added PaymentApiService

---
+++ SelfBuy/Services/PaymentApiService.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentApiService.swift
+//  SelfBuy
+//
+//  Created by Léo GIGT on 25/03/2020.
+//  Copyright © 2020 MaxenceMottard. All rights reserved.
+//
+
+import Foundation
+
+final class PaymentApiService {
+    let decoder = JSONDecoder()
+    
+    func createCard(card: CardPaymentDTO,completionHandler: @escaping (Result<CardPayment, Error>) -> Void) {
+        let request = StripeRequest(url: "https://api.stripe.com/v1/tokens")
+        
+        request
+            .setMethod(.POST)
+            .setBody(card)
+            .send(CardPayment.self) {
+                switch $0 {
+                case .success(let data):
+                    print(data)
+                case .failure(let error):
+                    print(error)
+                }
+        }
+    }
+}

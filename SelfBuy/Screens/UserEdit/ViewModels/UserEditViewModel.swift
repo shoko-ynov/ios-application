e052ca8f540e22df39ab66cbfc12fba4aec175dc
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-28 02:45:57 +0200

    Added the updateUser function

--- SelfBuy/Screens/UserEdit/ViewModels/UserEditViewModel.swift
+++ SelfBuy/Screens/UserEdit/ViewModels/UserEditViewModel.swift
@@ -15,28 +15,46 @@ protocol UserEditViewModelling {
     var value: BehaviorRelay<String> { get }
     var bag: DisposeBag { get }
     var userId: String { get }
-    
-    func updateUser() -> Void
+    var parameter: String { get }
+    var service: UserApiService { get }
+    func updateUser(valueName: String, value: String, userId: String, parameter: String)
 }
 
 final class UserEditViewModel: UserEditViewModelling {
+    var valueName: String
+    var value: BehaviorRelay<String>
+    var userId: String
+    var parameter: String
+    var service = UserApiService()
     var bag: DisposeBag
     
-    var valueName: String
-    
-    var value: BehaviorRelay<String>
-    
-    var userId: String
-    
-    
-    init(valueName: String, value: String, userId: String) {
+    init(valueName: String, value: String, userId: String, parameter: String) {
         self.valueName = valueName
         self.value = BehaviorRelay(value: value)
         self.userId = userId
+        self.parameter = parameter
         self.bag = DisposeBag()
+        self.service = UserApiService()
     }
     
-    func updateUser() {
+    func updateUser(valueName : String, value: String, userId: String, parameter: String) {
         
+        let user = UserEditDTO(
+               parameter: parameter,
+               value: value
+       )
+
+        service.updateUser(userData: user, id: userId, parameter: parameter){ (result) in
+            switch result {
+            case .success(let user):
+                print("success")
+                print(user)
+            case .failure(let error as NSError):
+                print(error)
+            default:
+                print("default")
+            }
+        }
     }
+
 }

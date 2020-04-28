97c5bc9d7566719923e461a24832a3bbafdadb48
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-28 02:44:46 +0200

    Added 'parameter' parameter to the UserData static data

--- SelfBuy/Screens/Account/ViewModels/AccountViewModel.swift
+++ SelfBuy/Screens/Account/ViewModels/AccountViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import RxSwift
 
 struct UserData {
-    let label, iconName: String
+    let label, iconName, parameter: String
 }
 
 protocol AccountViewModelling {
@@ -22,12 +22,12 @@ protocol AccountViewModelling {
 final class AccountViewModel: AccountViewModelling {
     
     let staticUserData = [
-        UserData(label: "Prénom", iconName: "person.fill"),
-        UserData(label: "Nom", iconName: "person.fill"),
-        UserData(label: "Email", iconName: "envelope.fill"),
-        UserData(label: "Adresse", iconName: "house.fill"),
-        UserData(label: "Ville", iconName: "house.fill"),
-        UserData(label: "Code Postal", iconName: "house.fill"),
+        UserData(label: "Prénom", iconName: "person.fill", parameter: "firstName"),
+        UserData(label: "Nom", iconName: "person.fill", parameter: "lastName"),
+        UserData(label: "Email", iconName: "envelope.fill", parameter: "mail"),
+        UserData(label: "Adresse", iconName: "house.fill", parameter: "address"),
+        UserData(label: "Ville", iconName: "house.fill", parameter: "city"),
+        UserData(label: "Code Postal", iconName: "house.fill", parameter: "postalCode"),
     ]
     
     let repository: UserRepository = UserRepository.shared

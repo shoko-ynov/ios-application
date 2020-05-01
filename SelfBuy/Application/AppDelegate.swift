c55c50e095be9866c58cbc974530b41f778469df
MaxenceMottard <mottard.maxence@gmail.com>
2020-05-01 16:27:05 +0200

    Block orientation to portrait

--- SelfBuy/Application/AppDelegate.swift
+++ SelfBuy/Application/AppDelegate.swift
@@ -30,6 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.

f032250617ca8f83f42d69c3b91f13792d490526
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-05-01 18:39:51 +0200

    Created OrderShippingViewModelling

---
+++ SelfBuy/Screens/Payment/ViewModels/OrderShippingViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  OrderShippingViewModel.swift
+//  SelfBuy
+//
+//  Created by Zakarya TOLBA on 01/05/2020.
+//  Copyright © 2020 MaxenceMottard. All rights reserved.
+//
+
+
+import RxSwift
+import Foundation
+
+protocol OrderShippingViewModelling {
+    var bag: DisposeBag { get }
+    
+    var firstName: BehaviorSubject<String> { get }
+    var lastName: BehaviorSubject<String> { get }
+    var address: BehaviorSubject<String> { get }
+    var postalCode: BehaviorSubject<String> { get }
+    var city: BehaviorSubject<String> { get }
+}
+
+final class OrderShippingViewModel: OrderShippingViewModelling {
+    var firstName: BehaviorSubject<String>
+    
+    var lastName: BehaviorSubject<String>
+    
+    var address: BehaviorSubject<String>
+    
+    var postalCode: BehaviorSubject<String>
+    
+    var city: BehaviorSubject<String>
+    
+    var bag: DisposeBag
+    
+    init() {
+        bag = DisposeBag()
+        let user = UserRepository.shared.getUser()
+        firstName = BehaviorSubject(value: user?.firstName ?? "")
+        lastName = BehaviorSubject(value: user?.lastName ?? "")
+        address = BehaviorSubject(value: user?.address ?? "")
+        postalCode = BehaviorSubject(value: (user?.postalCode)! as String)
+        city = BehaviorSubject(value: user?.city ?? "")
+    }
+}
+

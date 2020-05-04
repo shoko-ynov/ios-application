34cdc97e950289ef4e6da98b63c8e54f8bebf1d2
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-05-04 02:59:39 +0200

    Revert override dark user interface style

--- SelfBuy/Application/SceneDelegate.swift
+++ SelfBuy/Application/SceneDelegate.swift
@@ -27,7 +27,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
                 
         window?.rootViewController = tabBarVC
-        window?.overrideUserInterfaceStyle = .dark
         //window?.makeKeyAndVisible()
     }
 
